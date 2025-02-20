f62a0a4738898876708c499c99072bfcce6d6533
virinchilalwani <virinchiravindrakumar.lalwani@wmich.edu>
2025-02-19 22:04:38 -0500

    testing code

---
+++ testing.py
@@ -0,0 +1,42 @@
+from ultralytics import YOLO
+import os
+import cv2
+
+
+model = YOLO("runs/detect/train12/weights/best.pt")
+
+# temp_model = model
+
+# temp_model.model.names = {0: 'medicine 1',
+#                  1: 'medicine 2',
+#                  3: 'medicine 3',
+#                  4: 'medicine 4'}
+
+# model.names = {0: 'medicine 1',
+#                1: 'medicine 2',
+#                2: 'medicine 3',
+#                3: 'medicine 4'}
+
+video = cv2.VideoCapture(0)
+
+if not video.isOpened():
+    print("Error: Could not open vidio")
+    exit()
+
+while True:
+    ret, frame = video.read()
+
+    if not ret:
+        print("Error: Failed to capture frame")
+        break
+
+    results = model.predict(frame, conf=0.75)
+    annotated_frame = results[0].plot()
+
+    cv2.imshow('frame', annotated_frame)
+
+    if cv2.waitKey(1) & 0xFF == ord('q'):
+        break
+
+video.release()
+cv2.destroyAllWindows()
--- training.py
+++ training.py
@@ -8,4 +8,4 @@ model = YOLO("yolo11n.pt")
 data = os.path.join(os.getcwd(), "final_data/data.yaml")
 
 
-results = model.train(data=data, epochs=10, imgsz=640, device="mps", save=True).
+results = model.train(data=data, epochs=10, imgsz=640, device="mps", save=True)
